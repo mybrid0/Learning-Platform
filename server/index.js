1a6ca010b368daed7e96200e82ec2cdbe4ddeb53
Mia Speirs <miaspeirs5@gmail.com>
2023-11-13 13:51:09 +0000

    Setting up database

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// Setting up express app
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,9 +6,15 @@ require("dotenv").config();
 
 const db = require("./models");
 
+//built in middleware
+app.use(express.json());
+app.use(cors());
+
 //Routers
 
 //Commence Server
-app.listen(process.env.PORT || 3001, () => {
-  console.log("listening on http://localhost:3001");
+db.sequelize.sync().then(() => {
+  app.listen(process.env.PORT || 3001, () => {
+    console.log("listening on http://localhost:3001");
+  });
 });
